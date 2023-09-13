8692055153276a6f5723ab1fa95fa72730ab7cce
AlexSherrow <sherrowa226@gmail.com>
2023-09-13 14:46:29 -0400

    First backend

---
+++ src/main/java/com/susBackend/SusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.susBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/susBackend/SusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.susBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
